c3933fe0989d97c5e1631d940b26e0b4c8ca210e
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-03-17 20:29:33 +0900

    create test form

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,104 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>ガチャ最適化サービス</h1>
+  <form @submit="submit">
+    <section id="gachas">
+      <h2>引きたいガチャ</h2>
+      <ul>
+        <li v-for="(gacha, index) in gachas" :key="gacha.id">
+          <input
+            type="checkbox"
+            :id="gacha.id"
+            :value="gacha.value"
+            name="gachas"
+            v-model="checkedGachas"
+          />
+          <label :for="gacha.id">{{ gacha.value }}</label>
+          <label
+            v-if="checkedGachas.includes(gacha.value)"
+            :for="`${gacha.id}-least-times`"
+          >
+            <input
+              type="number"
+              :id="`${gacha.id}-least-times`"
+              name="leastTimes"
+              v-model="leastTimes[index]"
+            />
+            回
+          </label>
+        </li>
+      </ul>
+    </section>
+    <section id="max-cost">
+      <h2>限度額</h2>
+      <input
+        type="number"
+        id="max-cost-input"
+        name="maxCost"
+        v-model="maxCost"
+      />
+      <label for="max-cost-input">円</label>
+    </section>
+    <section id="modes">
+      <h2>モード</h2>
+      <ul>
+        <li v-for="mode in modes" :key="mode.id">
+          <input
+            type="radio"
+            :id="mode.id"
+            :value="mode.value"
+            name="mode"
+            v-model="checkedMode"
+          />
+          <label :for="mode.id">{{ mode.value }}</label>
+        </li>
+      </ul>
+    </section>
+    <input type="submit" value="送信" />
+  </form>
+  <p>result: {{ result }}</p>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      gachas: [
+        { id: "pazdora", value: "パズドラ" },
+        { id: "umamusume", value: "ウマ娘" },
+        { id: "monst", value: "モンスト" }
+      ],
+      checkedGachas: [],
+      leastTimes: [],
+      maxCost: null,
+      modes: [
+        { id: "mode-a", value: "モードA" },
+        { id: "mode-b", value: "モードB" },
+        { id: "mode-c", value: "モードC" }
+      ],
+      checkedMode: [],
+      result: ""
+    };
+  },
+  methods: {
+    async submit(event) {
+      event.preventDefault();
+
+      const response = await axios.post(process.env.VUE_APP_API_ENDPOINT, {
+        gachas: this.checkedGachas,
+        leastTimes: this.leastTimes,
+        maxCost: this.maxCost,
+        mode: this.checkedMode
+      });
+      const responseJson = JSON.parse(response.data.body);
+      console.log(responseJson);
+
+      this.result = Object.keys(responseJson).map(
+        key => `${key}: ${responseJson[key]}`
+      );
+    }
   }
 };
 </script>
